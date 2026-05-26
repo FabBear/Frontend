5e44cf8d878b7d076aaaef7b9e5ed6df58f483d1
becherryu <dkgus504@sookmyung.ac.kr>
2026-05-26 14:25:09 +0900

    [FEAT] #2 prettier 설정 및 import문 자동 정렬 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,17 @@
-import pluginVue from "eslint-plugin-vue";
-import {
-  defineConfigWithVueTs,
-  vueTsConfigs,
-} from "@vue/eslint-config-typescript";
-import skipFormatting from "@vue/eslint-config-prettier/skip-formatting";
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import pluginVue from 'eslint-plugin-vue';
 
 export default defineConfigWithVueTs(
   {
-    ignores: ["dist", "coverage", "node_modules"],
+    ignores: ['dist', 'coverage', 'node_modules'],
   },
-  pluginVue.configs["flat/recommended"],
+  pluginVue.configs['flat/recommended'],
   vueTsConfigs.recommended,
   {
     rules: {
-      "vue/multi-word-component-names": "off",
+      'vue/multi-word-component-names': 'off',
     },
   },
-  skipFormatting,
+  skipFormatting
 );
